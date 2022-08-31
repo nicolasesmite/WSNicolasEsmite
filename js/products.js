81e32795f3cfe45c240911dd03271319a4855e3f
nicolasesmite <nicolasemanuelesmitevaldez@gmail.com>
2022-08-31 00:09:42 -0300

    Filtro por costos operativo

--- js/products.js
+++ js/products.js
@@ -1,15 +1,18 @@
 const URL_AUTOS = "https://japceibal.github.io/emercado-api/cats_products/101.json"
 
 let categoriesList = [];
+let minCount = undefined;
+let maxCount = undefined;
 
 
-function showProductsList(categoriesList){
-    //console.log(categoriesAutosList.products)
+function showProductsList(){
     let htmlContentToAppend = ""
-
     for(let i = 0; i < categoriesList.products.length; i++){ 
-        htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action">
+        //establecemos filtros de busqueda segun costo
+        if (((minCount == undefined) || (minCount != undefined && categoriesList.products[i].cost >= parseInt(minCount))) &&
+           ((maxCount == undefined) || (maxCount != undefined && categoriesList.products[i].cost <= parseInt(maxCount)))){
+         htmlContentToAppend += `
+         <div class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
                     <img src="` + categoriesList.products[i].image + `" alt="product image" class="img-thumbnail">
@@ -25,22 +28,32 @@ function showProductsList(categoriesList){
 
                 </div>
             </div>
-        </div>
-        `
+         </div>`
+        }
     }
+
     document.getElementById(localStorage.getItem("catID")).innerHTML = htmlContentToAppend
 
 }
 
 
+
 document.addEventListener("DOMContentLoaded", function(){
-    getJSONData(URL_AUTOS).then(resultado => {
-        if (resultado.status == "ok"){
-            categoriesAutosList = resultado.data
-            showProductsList(categoriesAutosList)
+    getJSONData(URL_AUTOS).then(function(resultado){
+        if (resultado.status === "ok"){
+            categoriesList = resultado.data
+            
         }
         else {
             alert("JSON incorrecto")
         }
+    })
+
+    document.getElementById("rangeFilterCount").addEventListener("click",function(){
+            minCount = document.getElementById("rangeFilterCountMin".value);
+            maxCount = document.getElementById("rangeFilterCountMax".value);
+    
+            //traemos datos de input max,min
+            showProductsList()
     })
 })

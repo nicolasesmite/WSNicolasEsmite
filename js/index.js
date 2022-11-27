5943a3a10a54d0f11285b0173da1f5e3a8afa972
nicolasesmite <nicolasemanuelesmitevaldez@gmail.com>
2022-11-27 11:30:40 -0300

    Final 1.0

--- js/index.js
+++ js/index.js
@@ -2,28 +2,29 @@ var forms = document.querySelectorAll('.needs-validation')
 const email = document.getElementById("email")
 
 
-
-document.addEventListener("DOMContentLoaded", function () {
-
-
     
 
     forms[0].addEventListener('submit', function (event) {
-  
+
         forms[0].classList.add('was-validated')
-        datosFormulario = true
+        event.preventDefault()
+        event.stopPropagation()
+  
+    
         if (!forms[0].checkValidity()) {
             datosFormulario = false
-            event.preventDefault()
-            event.stopPropagation()
+            
         } else {
-    
             localStorage.setItem("usuario_loggeado", email.value)
-            window.location.href = 'pagina_principal.html'
+            location.href = "pagina_principal.html"
+
         }
+
+
+         
         
     })
-})
+
 
 
 
